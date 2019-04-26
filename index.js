df5c878d349b26dbfa3220c9b34493d8d2a0387c
Will Britton <will@undeveloper.com>
2019-04-26 16:09:06 +0100

    My solution. More lines = more bad.

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const { sendEmail } = require('./emailer');
 const { scores, emails } = require('./data');
 
-const notifyOfExamResults = () => {
-  // your solution should be implementated here
-  // console.log('data.scores', scores)
-  // console.log('data.emails', emails)
-};
+const notifyOfExamResults = () => emails.reduce((a, { name: l, email: h }) => ((b) => (sendEmail(h, (b > 50 ? ((c, d) => `Congratulations ${c}, you passed your exams with ${d}%!`) : ((e, f) => `Bad luck ${e}, you failed your exams with ${f}%.`))(((g) => `${g.charAt(0).toUpperCase()}${g.slice(1)}`)(l), b))) ? a : [...a, h])(((i, j) => i.filter(({ name: m }) => m === j).reduce((k, { score: n },_, { length: o }) => k + n / o, 0))(scores, l)), []);
 
 module.exports = notifyOfExamResults;
-
